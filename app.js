b41fa21cb7615f63c3c6fd39b35987377238601b
Matei Grigore <M.Grigore@student.tudelft.nl>
2023-08-25 12:44:34 +0200

    Feat: Add error template

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const campground = require("./models/campground");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -39,6 +40,9 @@ app.get("/campgrounds/new", (req, res) => {
 });
 
 app.post("/campgrounds", catchAsync(async (req, res, next) => {
+    if(!req.body.campground){
+        throw new ExpressError("Invalid campground data", 400)
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -68,8 +72,16 @@ app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
     res.redirect("/campgrounds");
 }));
 
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page not found", 404))
+})
+
 app.use((err, req, res, next) => {
-    res.send("Something went wrong")
+    const { statusCode = 500 } = err;
+    if(!err.message){
+        err.message = "Something went wrong"
+    }
+    res.status(statusCode).render("error", { err });
 })
 
 app.listen(3000, () => {
