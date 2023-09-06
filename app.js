e93718f86ae35d0f42518b50e756a62303c38a7c
Matei Grigore <M.Grigore@student.tudelft.nl>
2023-09-06 14:55:39 +0200

    Feat: Add creating review post route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const { campgroundSchema } = require("./schemas");
+const Review = require("./models/review");
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -79,6 +80,16 @@ app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
 }));
+
+app.post("/campgrounds/:id/reviews", catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
 
 app.all("*", (req, res, next) => {
     next(new ExpressError("Page not found", 404))
